6fb5fe130585e7a24f169025d22fa622ffb14378
Elliot Kirk <elliot.s.kirk@gmail.com>
2018-02-01 13:10:39 -0800

    Implemented 'put'

--- Routes/Account/Prss.js
+++ Routes/Account/Prss.js
@@ -6,8 +6,7 @@ var mysql = require('mysql');
 
 router.baseURL = '/Prss';
 
-/*
-*/
+
 router.get('/', function(req, res) {
    var email = req.session.isAdmin() && req.query.email ||
     !req.session.isAdmin() && req.session.email;
@@ -46,96 +45,7 @@ router.get('/', function(req, res) {
       });
 });
 
-
-/* Non-waterfall, non-validator, non-db automation version
-router.post('/', function(req, res) {
-   var body = req.body;
-   var admin = req.session && req.session.isAdmin();
-   var errorList = [];
-   var qry;
-   var noPerm;
-   var cnnConfig = {
-      host     : '127.0.0.1',
-      user     : 'cstaley',
-      password : 'secret',
-      database : 'CHSdb'
-   };
-
-   req.cnn.release();  // Since we're not using that
-
-   if (admin && !body.password)
-      body.password = "*";                       // Blocking password
-   body.whenRegistered = new Date();
-
-   // Check for fields
-   if (!body.hasOwnProperty('email'))
-      errorList.push({tag: "missingField", params: "email"});
-   if (!body.hasOwnProperty('password'))
-      errorList.push({tag: "missingField", params: "password"});
-   if (!body.hasOwnProperty('role'))
-      errorList.push({tag: "missingField", params: "role"});
-
-   // Do these checks only if all fields are there
-   if (!errorList.length) {
-      noPerm = body.role === 1 && !admin;
-      if (!body.termsAccepted)
-         errorList.push({tag: "noTerms"});
-      if (body.role < 0)
-         errorList.push({tag: "badVal", param: "role"});
-   }
-
-   // Post errors, or proceed with data fetches
-   if (noPerm)
-      res.status(403).end();
-   else if (errorList.length)
-      res.status(400).json(errorList);
-   else {
-      var cnn = mysql.createConnection(cnnConfig);
-
-      // Find duplicate Email if any.
-      cnn.query(qry = 'select * from Person where email = ?', body.email,
-      function(err, dupEmail) {
-         if (err) {
-            cnn.destroy();
-            res.status(500).json("Failed query " + qry);
-         }
-         else if (dupEmail.length) {
-            cnn.destroy();
-            res.status(400).json({tag: "dupEmail"});
-         }
-         else { // No duplicate, so make a new Person
-            body.termsAccepted = body.termsAccepted && new Date();
-            cnn.query(qry = 'insert into Person set ?', body,
-            function(err, insRes) {
-               cnn.destroy();
-               if (err)
-                  res.status(500).json("Failed query " + qry);
-               else
-                  res.location(router.baseURL + '/' + insRes.insertId).end();
-            });
-          }
-      });
-   }
-});
-*/
-
-// Much nicer versions
-router.get('/', function(req, res) {
-   var email = req.session.isAdmin() && req.query.email ||
-    !req.session.isAdmin() && req.session.email;
-
-   var handler = function(err, prsArr) {
-      res.json(prsArr);
-      req.cnn.release();
-   };
-
-   if (email)
-      req.cnn.chkQry('select id, email from Person where email = ?', [email],
-       handler);
-   else
-      req.cnn.chkQry('select id, email from Person', handler);
-});
-
+// waterfall implementation of post
 router.post('/', function(req, res) {
    var vld = req.validator;  // Shorthands
    var body = req.body;
@@ -146,16 +56,30 @@ router.post('/', function(req, res) {
       body.password = "*";                       // Blocking password
    body.whenRegistered = new Date();
 
+   // takes in two parameters:
+   //    - array of callback functions
+   //    - another function that is called if any errors occur (consider it a try/catch block)
    async.waterfall([
+   // cb is the callback function taking in two parameters
+   //    - error
+   //    - result
    function(cb) { // Check properties and search for Email duplicates
+      // vld.hasFields returns true only if all the fields are there
+      // if vld.hasFields returns false, none of the subsequent chain calls are executed - 
+      //    the cb parameter gets called and the validator calls cb(this), this is truthy,
+      //    and the final callback is executed, releasing the connection
       if (vld.hasFields(body, ["email", "password", "role"], cb) &&
        vld.chain(body.role === 0 || admin, Tags.noPermission)
        .chain(body.termsAccepted || admin, Tags.noTerms)
        .check(body.role >= 0, Tags.badValue, ["role"], cb)) {
+         // the result of the cnn.chkQry call is passed into the cb parameter which - in a async.waterfall - 
+         // ends up just being the next function in the function array
+         // SO cnn.chkQry(query, param, cb) -> function(existingPrss, fields, cb)
          cnn.chkQry('select * from Person where email = ?', body.email, cb);
       }
    },
    function(existingPrss, fields, cb) {  // If no duplicates, insert new Person
+      // null needs to be passed or else cb would become the "params" field and we can lose connections
       if (vld.check(!existingPrss.length, Tags.dupEmail, null, cb)) {
          body.termsAccepted = body.termsAccepted && new Date();
          cnn.chkQry('insert into Person set ?', body, cb);
@@ -164,51 +88,69 @@ router.post('/', function(req, res) {
    function(result, fields, cb) { // Return location of inserted Person
       res.location(router.baseURL + '/' + result.insertId).end();
       cb();
+      // ^-- why is this guy here?
+      // if you don't include that, the final error checking function will never be called
+      // not only is the error check function good for error checking, it is also somewhat of
+      // a "finally" statement. If you don't call the final callback you would never release
+      // the connection
    }],
+   // one error handler to rule them all
    function(err) {
       cnn.release();
    });
 });
 
-// router.put('/:id', function(req, res) {
-//    var vld = req.validator;
-//    var body = req.body;
-//    var admin = req.session && req.session.isAdmin();
-//    var ccn = req.cnn;
+// waterfall implementation of put
+router.put('/:id', function(req, res) {
+   var vld = req.validator;
+   var body = req.body;
+   var admin = req.session.isAdmin();
+   var cnn = req.cnn;
+   var id = req.params.id
 
-//    async.waterfall({
-//    function(cb) { // Check to see if the user is attempting to modify improper properties
-//       if (vld.hasFields(body, ["termsAccepted", "whenRegistered"], cb)) {
-//          vld.chain(!body.termsAccepted || body.termsAccepted, Tags.forbiddenField)
-//          .chain(!body.whenRegistered || body.whenRegistered, Tags.forbiddenField)
-//          .check(body.termsAccepted || body.whenRegistered, Tags.forbiddenField, ["fields"], cb);
-//       }
-//    },
-//    function(cb) {
+   async.waterfall([
+   function(cb) {
+      if (vd.checkPrsOK(id, cb) && 
+         vld.chain(!("termsAccepted" in body), Tags.forbiddenField, ['termsAccepted'])
+         .chain(!("whenRegistered" in body), Tags.forbiddenField, ['whenRegistered'])
+         .chain(!("password" in body) || body.password, Tags.badValue, ['password'])
+         .chain(!("password" in body) || ("oldPassword" in body) || admin, Tags.noOldPwd, ['password'])
+         .check(body.role && admin, Tags.badValue, ['role'], cb)) 
+            cnn.chkQry('select * from Person where id = ?', id, cb);
+   },
+   function(prss, fields, cb) {
+      if (vld.check(prss.length, Tag.notFound, null, cb) &&
+          vld.check(admin || !("password" in body) ||
+          body.oldPassword === prs[0].password, Tags.oldPwdMismatch, null, cb)) {
+         delete body.oldPassword;
+         cnn.chkQry("update Person set ? where id = ?", [body, id], cb);
+      }
+   }],
+   function(err) {
+      if (!err)
+         res.status(200).end();
+      cnn.release();
+   });
+});
 
-//    }
-//    })
-
-//    if (vld.checkPrsOK(req.params.id)) {
-      
-//    }
-// });
-
-
+// waterfall implementation of get
 router.get('/:id', function(req, res) {
    var vld = req.validator;
+   var cnn = req.cnn;
 
-   if (vld.checkPrsOK(req.params.id)) {
-      req.cnn.query('select * from Person where id = ?', [req.params.id],
-      function(err, prsArr) {
-         if (vld.check(prsArr.length, Tags.notFound))
+   async.waterfall([
+      function(cb) {
+         if (vld.checkPrsOK(req.params.id, cb)) 
+            cnn.chkQry('select * from Person where id = ?', [req.params.id], cb);
+      },
+      function(prsArr, fields, cb) {
+         if (vld.check(prsArr.length, Tags.notFound, null, cb))
             res.json(prsArr);
-         req.cnn.release();
+            cb();
+      }],
+      function(err) {
+         cnn.release();
       });
-   }
-   else {
-      req.cnn.release();
-   }
 });
 
 router.delete('/:id', function(req, res) {
